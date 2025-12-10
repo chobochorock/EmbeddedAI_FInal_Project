a1875472095b6cc481bea86fd4efbe46268e2669
chobochorock <choi102578@gmail.com>
2025-12-10 21:48:32 +0900

    feat: resizing camera image size

--- yolov8/run_jetson_fixed.py
+++ yolov8/run_jetson_fixed.py
@@ -54,8 +54,8 @@ def main():
         sensor_id=0,
         capture_width=1280,
         capture_height=720,
-        display_width=640,
-        display_height=480,
+        display_width=128,
+        display_height=128,
         framerate=30,
         flip_method=0,
     ):
